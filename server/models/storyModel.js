ae45552460b52875d20ea09796bfa5179446b292
Kaayo <caiokaea@gmail.com>
2025-09-11 11:10:50 -0300

    fix: storyModel fixed

--- server/models/storyModel.js
+++ server/models/storyModel.js
@@ -1,1 +1,22 @@
-c
+import { getConnection } from "../config/oracle.js";
+
+export const create = async (story, userId) => {
+  const conn = await getConnection();
+
+  try {
+    const result = await conn.execute(
+      `INSERT INTO cyberpunk_story_history (id, story_content, created_at, id_user) 
+       VALUES (seq_story.nextval, :story_content, :created_at, :id_user)`,
+      {
+        story_content: story,
+        created_at: new Date(),
+        id_user: userId,
+      },
+      { autoCommit: true }
+    );
+
+    return result.rowsAffected;
+  } finally {
+    await conn.close();
+  }
+};
